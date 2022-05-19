35b564b4f542e1fb110e53eb4b2a1ab07831c261
Seoyeon Yoon <s2013@e-mirim.hs.kr>
2022-05-19 11:11:54 +0900

    #1 클래스 생성 예제 Alt + Enter의 활용법 C#이 자동완성을 해주어 편하다.

---
+++ CSClass/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSClass
+{
+    class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+
+        public void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
+    }
+}
--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -10,6 +10,9 @@ namespace CSClass
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            car.SetInTime();
+            car.SetOutTime();
         }
     }
 }
