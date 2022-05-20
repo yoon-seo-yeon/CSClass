8dbb7eccefad5d1761b29f0bbff7c3f40ccec126
Seoyeon Yoon <s2013@e-mirim.hs.kr>
2022-05-20 10:03:06 +0900

    #1 XML 주석 달기 ///로 달 수 있다.

--- CSClass/Car.cs
+++ CSClass/Car.cs
@@ -2,12 +2,28 @@
 
 namespace CSClass
 {
+    /// <summary>
+    /// 자동차 주차장 입차, 출차 시간 등을 저장하는 클래스입니다.
+    /// </summary>
     class Car
     {
         int carNumber;
         DateTime inTime;
         DateTime outTime;
 
+        /// <summary>
+        /// 뭔가 입력받고 출력합니다.
+        /// </summary>
+        /// <param name="foo">뭔가 변수</param>
+        /// <returns>뭔가 리턴값</returns>
+        public int some(int foo)
+        {
+            return foo * 10;
+        }
+
+        /// <summary>
+        /// 자동차 입차 시간을 지정합니다.
+        /// </summary>
         public void SetInTime()
         {
             this.inTime = DateTime.Now;
